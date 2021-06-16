24ae7409504355d7aedc7bd9b77264765d15cbce
Liu Hui <ilupin@users.noreply.github.com>
2021-06-17 05:48:36 +0800

    do not override overriding-terminal-local-map in non-composition state (#163)
    * do not override overriding-terminal-local-map in non-composition state
    
    * do not override overriding-local-map too

--- rime.el
+++ rime.el
@@ -755,6 +755,12 @@ By default the input-method will not handle DEL, so we need this command."
   (setq rime--current-input-key key)
   (when (rime--rime-lib-module-ready-p)
     (if (or (rime--text-read-only-p)
+            (and (not rime-active-mode)
+                 (or (and overriding-terminal-local-map
+                          (or (not (eq (cadr overriding-terminal-local-map)
+                                       universal-argument-map))
+                              (lookup-key overriding-terminal-local-map (vector key))))
+                     overriding-local-map))
             (and (not (rime--should-enable-p))
                  (not (rime--has-composition (rime-lib-get-context)))))
         (list key)
